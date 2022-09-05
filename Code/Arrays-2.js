d9c430a3c75617161adacc3ba7ae25caf1b15a3a
Bodms <88928856+Bodms@users.noreply.github.com>
2022-09-05 12:59:27 +0530

    Update Arrays-2.js

--- Code/Arrays-2.js
+++ Code/Arrays-2.js
@@ -435,52 +435,52 @@
 // find total exp => workexp + deptexp, of companies 
 // in which he was a permanent employee
 
-var sudhakarData =
-[
-  {
-    id: 111,
-    name: "Chelsea Foster",
-    workExp: 7,
-    deptExp: 6,
-    isPermanent: true,
-  },
-  {
-    id: 102,
-    name: "Aggie Sparling",
-    workExp: 13,
-    deptExp: 10,
-    isPermanent: false,
-  },
-  {
-    id: 123,
-    name: "Timmy Matthews",
-    workExp: 23,
-    deptExp: 15,
-    isPermanent: false,
-  },
-  {
-    id: 66,
-    name: "Emmet Foster",
-    workExp: 22,
-    deptExp: 9,
-    isPermanent: true,
-  },
-  {
-    id: 89,
-    name: "Brent Dolan",
-    workExp: 16,
-    deptExp: 12,
-    isPermanent: true,
-  },
-];
+// var sudhakarData =
+// [
+//   {
+//     id: 111,
+//     name: "Chelsea Foster",
+//     workExp: 7,
+//     deptExp: 6,
+//     isPermanent: true,
+//   },
+//   {
+//     id: 102,
+//     name: "Aggie Sparling",
+//     workExp: 13,
+//     deptExp: 10,
+//     isPermanent: false,
+//   },
+//   {
+//     id: 123,
+//     name: "Timmy Matthews",
+//     workExp: 23,
+//     deptExp: 15,
+//     isPermanent: false,
+//   },
+//   {
+//     id: 66,
+//     name: "Emmet Foster",
+//     workExp: 22,
+//     deptExp: 9,
+//     isPermanent: true,
+//   },
+//   {
+//     id: 89,
+//     name: "Brent Dolan",
+//     workExp: 16,
+//     deptExp: 12,
+//     isPermanent: true,
+//   },
+// ];
 
-var permEmp = sudhakarData.filter(function (comp){
-    return comp.isPermanent
-})
+// var permEmp = sudhakarData.filter(function (comp){
+//     return comp.isPermanent
+// })
 
-var sumOfTotalExp = permEmp.reduce(function(acc,currEmp){
-    return acc+currEmp.deptExp+currEmp.workExp
-},0)
+// var sumOfTotalExp = permEmp.reduce(function(acc,currEmp){
+//     return acc+currEmp.deptExp+currEmp.workExp
+// },0)
 
-console.log(sumOfTotalExp)
+// console.log(sumOfTotalExp)
 
